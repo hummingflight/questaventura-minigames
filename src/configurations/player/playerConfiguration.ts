0969ad9cf09fa11181e8d341f8b7a8f629334e89
Max Maldonado <nuup20@gmail.com>
2024-01-09 20:48:41 -0600

    documentation

--- src/configurations/player/playerConfiguration.ts
+++ src/configurations/player/playerConfiguration.ts
@@ -1,13 +1,28 @@
 export class PlayerConfiguration
 {
+  /**
+   * The key of the image of the player.
+   */
   public spriteKey: string;
 
+  /**
+   * The jump velocity of the player (pixels per second).
+   */
   public jumpVelocity: number;
 
+  /**
+   * The scale of the player in the X axis.
+   */
   public scaleX: number;
 
+  /**
+   * The scale of the player in the Y axis.
+   */
   public scaleY: number;
 
+  /**
+   * The movement velocity in the X axis of the player (pixels per second).
+   */
   public movementVelocity: number;
 
   /**

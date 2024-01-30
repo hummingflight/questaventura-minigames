57f4f663bccdc09a3a494eca09a0aafddf5442a1
Max Maldonado <nuup20@gmail.com>
2024-01-30 07:53:57 -0600

    updates

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const GameConfig: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 },
+      gravity: { y: 300 },
       debug: true
     }
   },
